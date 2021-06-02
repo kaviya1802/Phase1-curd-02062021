eebdfe3b8d071509cc60e2e8bb97903bc6f70360
kaviya1802 <kaviyakaviya182@gmail.com>
2021-06-02 13:33:14 +0000

    crud

--- src/com/simplilearn/web/UpdateProductwithParam.java
+++ src/com/simplilearn/web/UpdateProductwithParam.java
@@ -3,6 +3,7 @@ package com.simplilearn.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Properties;
 
@@ -15,16 +16,16 @@ import javax.servlet.http.HttpServletResponse;
 import com.simplilearn.connection.DBConnection;
 
 /**
- * Servlet implementation class AddProduct
+ * Servlet implementation class UpdateProduct
  */
-@WebServlet("/AddProduct")
-public class AddProduct extends HttpServlet {
+@WebServlet("/UpdateProductwithParam")
+public class UpdateProductwithParam extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public AddProduct() {
+    public UpdateProductwithParam() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -34,20 +35,8 @@ public class AddProduct extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		
-		response.sendRedirect("addproduct.html");
-		return;
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 try {
 			
-			String name =  request.getParameter("productName");
-			String price = request.getParameter("productPrice");
 			PrintWriter out = response.getWriter();
 			
 			//load properties from config
@@ -58,14 +47,15 @@ try {
 			//fetch properties from config and create connection
 			DBConnection conn = new DBConnection(properties.getProperty("url"), properties.getProperty("username")
 					, properties.getProperty("password"));
-		
 			
-			String query = "insert into eproduct(name, price) values(?, ?);";
+			String query = "update eproduct set name = ? where  id = ?";
 			//create statement
 			PreparedStatement pstm = conn.getConnection().prepareStatement(query);
 			
-			pstm.setString(1, name);
-			pstm.setDouble(2, Double.parseDouble(price));
+			pstm.setString(1, "HP Laptop G450");
+			pstm.setInt(2, 2);
+			
+			//Execute query
 			
 			int num = pstm.executeUpdate();
 			
@@ -74,7 +64,7 @@ try {
 			if(num>0) {
 				out.println("No of Rows affected : "+num);
 			}else {
-				out.println("The product cannot be added");
+				out.println("The product cannot be updated");
 				}
 			out.println("</body></html>");
 			
@@ -83,6 +73,15 @@ try {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
